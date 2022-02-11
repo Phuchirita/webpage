897d98cc0573dd1a2f8f6447e380bb2afd80dfb8
Phuchirita <phuchirita.b@gmail.com>
2022-02-11 18:05:48 +0700

    git commit

--- webpage.php
+++ webpage.php
@@ -6,13 +6,19 @@
 <body>
 
 <div>
-  <h2 class="txtheader">BERYLLIUM</h2>
+<div class="txtheader">BERYLLIUM</div>
+<ul class="navi">
+  <li>HOME</li>
+  <li>ABOUT</li>
+  <li>SERVICES</li>
+  <li>DROPDOWN</li>
+  <li>PORTFOLIO</li>
+  <li>CONTACT</li>
+</ul>
 </div>
 
 <div class="row">
   <div class="column side" style="background-color:#aaa;">Column</div>
-  <div class="column middle" style="background-color:#bbb;">Column</div>
-  <div class="column side" style="background-color:#ccc;">Column</div>
 </div>
 
 <div class="footer">
