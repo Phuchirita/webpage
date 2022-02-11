19f076ef304d8687aa13ade05bf7602ff26479ec
Phuchirita <phuchirita.b@gmail.com>
2022-02-11 17:41:33 +0700

    git commit

--- webpage.php
+++ webpage.php
@@ -1,9 +1,22 @@
 <!DOCTYPE html>
 <html>
+<head>
+    <link rel = "stylesheet" type = "text/css" href = "style.css"> 
+</head>
 <body>
 
-<h1>My First Heading</h1>
-<p>My first.</p>
+<div>
+  <h2 class="txtheader">BERYLLIUM</h2>
+</div>
 
+<div class="row">
+  <div class="column side" style="background-color:#aaa;">Column</div>
+  <div class="column middle" style="background-color:#bbb;">Column</div>
+  <div class="column side" style="background-color:#ccc;">Column</div>
+</div>
+
+<div class="footer">
+  <p>Footer</p>
+</div>
 </body>
 </html>
