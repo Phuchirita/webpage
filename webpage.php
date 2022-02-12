4e209cd11086d647dcb06a67a285753288d274cd
Phuchirita <phuchirita.b@gmail.com>
2022-02-12 17:06:11 +0700

    t

--- webpage.php
+++ webpage.php
@@ -62,13 +62,13 @@
 </div>
 
 <div style="text-align:center">
-  <span class="dot" onclick="currentSlide(1)"></span> 
-  <span class="dot" onclick="currentSlide(2)"></span> 
-  <span class="dot" onclick="currentSlide(3)"></span> 
+  <span class="dot" onclick="currentSlide2(1)"></span> 
+  <span class="dot" onclick="currentSlide2(2)"></span> 
+  <span class="dot" onclick="currentSlide2(3)"></span> 
 </div>
 
-<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-<a class="next" onclick="plusSlides(1)">&#10095;</a>
+<a class="prev" onclick="plusSlides2(-1)">&#10094;</a>
+<a class="next" onclick="plusSlides2(1)">&#10095;</a>
 </div>
 </div>
 
