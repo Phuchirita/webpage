61450a74bc819b4787debfd348c95693776a5b98
Phuchirita <phuchirita.b@gmail.com>
2022-02-12 14:24:02 +0700

    t

--- webpage.php
+++ webpage.php
@@ -15,7 +15,7 @@
 <a>CONTACT</a>
 </div>
 
-<div>
+<div style="width: 60%;">
 <div class="slideshow-container">
 
 <div class="mySlides fade">
@@ -58,14 +58,10 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
