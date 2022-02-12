a488e6e98f4ce3a6167e9d550ea0ed94ba6a9f67
Phuchirita <phuchirita.b@gmail.com>
2022-02-12 14:20:05 +0700

    t

--- webpage.php
+++ webpage.php
@@ -16,11 +16,60 @@
 </div>
 
 <div>
-  <div>Column</div>
+<div class="slideshow-container">
+
+<div class="mySlides fade">
+  <img src="slider_1.jpg" style="width:100%">
+  <div class="text">Arhitecture#1</div>
+</div>
+
+<div class="mySlides fade">
+  <img src="slider_2.jpg" style="width:100%">
+  <div class="text">Arhitecture#2</div>
+</div>
+
+<div class="mySlides fade">
+  <img src="slider_3.jpg" style="width:100%">
+  <div class="text">Arhitecture#3</div>
+</div>
+
+<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+<a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+</div>
 </div>
 
 <div class="footer">
   <p>Footer</p>
 </div>
+
+<script>
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+</script>
 </body>
 </html>
