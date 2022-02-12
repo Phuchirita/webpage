eed2e8ed95edbbf7b324a879573b7b3d5b694b7c
Phuchirita <phuchirita.b@gmail.com>
2022-02-12 13:07:33 +0700

    git commit

--- webpage.php
+++ webpage.php
@@ -4,18 +4,9 @@
     <link rel = "stylesheet" type = "text/css" href = "style.css"> 
 </head>
 <body>
+    
 <div>
 <div class="txtheader">BERYLLIUM</div>
-<div class="navi">
-<ul>
-  <li>HOME</li>
-  <li>ABOUT</li>
-  <li>SERVICES</li>
-  <li>DROPDOWN</li>
-  <li>PORTFOLIO</li>
-  <li>CONTACT</li>
-</ul>
-</div>
 </div>
 
 <div class="row">
