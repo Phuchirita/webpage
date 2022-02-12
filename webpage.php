2e5a4d7c72d9d30f4e04a86a3a5f0c1c471132ae
Phuchirita <phuchirita.b@gmail.com>
2022-02-12 17:23:09 +0700

    t

--- webpage.php
+++ webpage.php
@@ -5,6 +5,7 @@
     <!--<script type="text/javascript" src="scriptaction.js"></script>-->
 </head>
 <body>
+<div style="width: 100%;">
 <!--Navigation bar-->
 <div class="txtheader">
 <a>BERYLLIUM</a>
@@ -46,22 +47,25 @@
 </div>
 <br>
 <!--Slide Image dot-->
-<div>
+
 <div class="slides-container">
 
+<div >
 <div class="SlideImage2 fade">
   <img src="img_1.jpg" style="width:50%">
   <div>New York Area</div>
   <div>New York</div>
 </div>
 </div>
-<div class="slides-container">
+
+<div>
 <div class="SlideImage2 fade">
   <img src="img_2.jpg" style="width:50%">
   <div>Eagel Park</div>
   <div>London</div>
 </div>
 </div>
+
 <div style="text-align:center">
   <span class="dot" onclick="currentSlide2(1)"></span> 
   <span class="dot" onclick="currentSlide2(2)"></span> 
@@ -70,13 +74,14 @@
 
 <a class="prev" onclick="plusSlides2(-1)">&#10094;</a>
 <a class="next" onclick="plusSlides2(1)">&#10095;</a>
-
 </div>
+
 
 <!-- Paragrap Text 2 column -->
 <!--Footer-->
 <div class="footer">
   <p></p>
+</div>
 </div>
 <!--Script Slide Image Architecture-->
 <script type="text/javascript">
