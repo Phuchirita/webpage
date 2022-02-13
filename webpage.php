6bebab07982ba0185e0c8ba5d83f784685e59415
Phuchirita <phuchirita.b@gmail.com>
2022-02-13 17:32:23 +0700

    commit

--- webpage.php
+++ webpage.php
@@ -2,19 +2,20 @@
 <html>
 <head>
     <link rel = "stylesheet" type = "text/css" href = "style.css"> 
-    <!--<script type="text/javascript" src="scriptaction.js"></script>-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
-<body>
+<body style="width: 100%;">
 <!--Navigation bar-->
 <div class="txtheader">
-<a>BERYLLIUM</a>
-<a>HOME</a>
-<a>ABOUT</a>
-<a>SERVICES</a>
-<a>DROPDOWN</a>
-<a>PORTFOLIO</a>
-<a>CONTACT</a>
+<a style="text-align: left;padding-right:350px;font-weight: bold;">BERYLLIUM</a>
+<a style="font-size: 15px;padding-right:10px;">HOME</a>
+<a style="font-size: 15px;padding-right:10px;">ABOUT</a>
+<a style="font-size: 15px;padding-right:10px;">SERVICES<i class="fa fa-angle-down" style="padding-left:5px;"></i></a>
+<a style="font-size: 15px;padding-right:10px;">DROPDOWN<i class="fa fa-angle-down" style="padding-left:5px;"></i></a>
+<a style="font-size: 15px;padding-right:10px;">PORTFOLIO</a>
+<a style="font-size: 15px;">CONTACT</a>
 </div>
+
 <!--Slide Image Architecture-->
 <div class="slideshow-container">
 
@@ -37,59 +38,195 @@
 <a class="next" onclick="plusSlides(1)">&#10095;</a>
 </div>
 <br>
+
 <!--Paragrapt Work-->
-<div style="padding-bottom:30px;">
+<div style="padding-bottom:90px;padding-top:50px;">
   <div class="txth">Work</div>
   <br>
   <div class="txtp">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus placerat enim</div>
   <div class="txtp">et urna sagittis, rhoncus euismod erat tincidunt. Donec tincidunt Volutpat erat.</div>
 </div>
 <br>
-<!--Slide Image dot-->
-<!--
-<div class="slides-container">
-  <div class="slide">
-      <img src="img_1.jpg">
-      <div>New York Area</div>
-      <div>New York</div>
+<!--Slide Image dot
+<div class="slideshow-container" style="padding-bottom: 50px;">
+  <div class="rows">
+    <div class="columns" style="width: 100px;">
+      <div class="SlideImage2 fade">
+        <img class="imgs" src="img_1.jpg" style="padding-bottom: 30px;">
+        <div class="font" style="font-weight: bold;font-size:20px;padding-bottom:10px;">New York Area</div>
+        <div class="font" style="color: gray;">New York</div>
+      </div> 
+    </div>
+    <div class="columns" style="width: 100px;">
+      <div class="SlideImage2 fade">
+        <img class="imgs" src="img_2.jpg" style="padding-bottom: 10px;">
+        <p class="font" style="font-weight: bold;font-size:20px;">Eagel Park</p>
+        <p class="font" style="color: gray;">London</p>
+      </div> 
+    </div>
+    <div class="columns" style="width: 100px;">
+      <div class="SlideImage2 face">
+        <img class="imgs" src="img_3.jpg" style="padding-bottom: 10px;">
+        <p class="font" style="font-weight: bold;font-size:20px;">Nationals Park</p>
+        <p class="font" style="color: gray;">Frann, Paris</p>
+      </div> 
+    </div>
   </div>
-  <div class="slide">
-      <img src="img_2.jpg">
-      <div>Eagel Park</div>
-      <div>London</div>
-  </div>
-<div style="text-align:center">
+  <div style="text-align:center">
   <span class="dot" onclick="currentSlide2(1)"></span> 
   <span class="dot" onclick="currentSlide2(2)"></span> 
   <span class="dot" onclick="currentSlide2(3)"></span> 
 </div>
-
-<a class="prev" onclick="plusSlides2(-1)">&#10094;</a>
-<a class="next" onclick="plusSlides2(1)">&#10095;</a>
-
+  <a class="prev" onclick="plusSlides2(-1)">&#10094;</a>
+  <a class="next" onclick="plusSlides2(1)">&#10095;</a>
 </div>
 -->
+<div class="carousel" style="padding-bottom: 60px;">
+<a class="prev" onclick="slideRight(this);">&#10094;</a>
+  <div class="rows flt-left">
+   <div class="row-container">
+  <div class="containers">
+    <img class="imgs" src="img_1.jpg" />
+    <p class="font" style="font-weight: bold;font-size:20px;">New York Area</p>
+    <p class="font" style="color: gray;">New York</p>
+  </div>
+  <div class="containers">
+    <img class="imgs" src="img_2.jpg" />
+    <p class="font" style="font-weight: bold;font-size:20px;">Eagel Park</p>
+    <p class="font" style="color: gray;">London</p>
+  </div>
+  <div class="containers">
+    <img class="imgs" src="img_3.jpg" />
+    <p class="font" style="font-weight: bold;font-size:20px;">Nationals Park</p>
+    <p class="font" style="color: gray;">Frann, Paris</p>
+  </div>
+    <div class="clear"></div>
+    </div>
+    
+    </div>
+    <a class="next" onclick="slideLeft(this);">&#10095;</a>
+    <div class="clear"></div>
+   
+</div> 
+
+
 
 <!-- Paragrap Text 2 column -->
-<div class="row" style="width:auto;">
-  <div class="column side">
-    <h2>Side</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit..</p>
+<div class="row">
+  <!-- News -->
+  <div class="column">
+    <h1 class="hd">News</h1>
+    <p class="date">SEPTEMBER 10,2016</p>
+    <p class="subject">Manila Bridge Re-construction<p>
+    <p class="detail">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus Placerat enim et urna sagittis, rhoncus euismod...</p>
+    <hr style="width:100%;text-align:center;">
+    <p class="date">SEPTEMBER 10,2016</p>
+    <p class="subject">Manila Bridge Re-construction<p>
+    <p class="detail">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus Placerat enim et urna sagittis, rhoncus euismod...</p>
+    <hr style="width:100%;text-align:center;">
+    <p class="date">SEPTEMBER 10,2016</p>
+    <p class="subject">Manila Bridge Re-construction<p>
+    <p class="detail">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus Placerat enim et urna sagittis, rhoncus euismod...</p> 
+    <button class="buttons button1">More News</button>
   </div>
-  
-  <div class="column middle">
-    <h2>Main Content</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
+  <!-- Testimonials -->
+  <div class="column" style="width:350px;padding-left:120px;">
+    <h2 class="hd">Testimonials</h2>
+    <p class="detail2">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus placerat enim et urna  sagittis, rhoncus euismod erat tincidunt. Donec tincidunt volupat erat."</p>
+    <p class="name">- John Doe Dueller</p>
+    <br>
+    <h2 class="hd">Testimonials</h2>
+    <p class="detail2">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus placerat enim et urna  sagittis, rhoncus euismod erat tincidunt. Donec tincidunt volupat erat."</p>
+    <p class="name">- John Doe Dueller</p>
   </div>
-  
 </div>
 
+<!-- Paragrap before footer -->
+<hr style="width:100%;text-align:center;">
+<div class="rowp" style="padding-top: 30px;">
+  <div class="columnp">
+    <div class="rowc">
+      <div class="columnc" style="width: 60px;">
+        <i class="fa fa-file-zip-o" style="font-size:40px"></i>
+      </div>
+      <div class="columnc" style="width: 260px;padding-left:10px;"> 
+        <h1 class="hdf">ARCHITECT</h1>
+        <p class="detailf">Lorem ipsum dolor sit ameteista,consectetur is not elitistaris.</p>
+        <a class="link">LEARN MORE</a>
+      </div>
+    </div>
+  </div>
+  <div class="columnp">
+  <div class="rowc">
+      <div class="columnc" style="width: 60px;">
+        <i class="fa fa-file-text-o" style="font-size:40px"></i>
+      </div>
+      <div class="columnc" style="width: 260px;padding-left:10px;"> 
+        <h1 class="hdf">PLANNING</h1>
+        <p class="detailf">Lorem ipsum dolor sit ameteista,consectetur is not elitistaris.</p>
+        <a class="link">LEARN MORE</a>
+      </div>
+    </div>
+  </div>
+  <div class="columnp">
+  <div class="rowc">
+      <div class="columnc" style="width: 60px;">
+        <i class="fa fa-calendar-o" style="font-size:40px"></i>
+      </div>
+      <div class="columnc" style="width: 260px;padding-left:10px;"> 
+        <h1 class="hdf">PARK & EVENTS</h1>
+        <p class="detailf">Lorem ipsum dolor sit ameteista,consectetur is not elitistaris.</p>
+        <a class="link">LEARN MORE</a>
+      </div>
+    </div>
+  </div>
+</div>
 
 
 <!--Footer-->
-<div class="footer">
-  <p></p>
+<div class="footer rowfoot">
+  <div class="rowf" style="padding-top:40px;padding-bottom:40px;">
+    <div class="columnfoot" style="width: 400px;">
+      <h1 class="headder1">Beryllium</h1>
+      <p class="text1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, Phasellus placerat enim et urna sagittis, rhoncus euismod.</p>
+    </div>
+    <div class="columnfoot" style="padding-left:30px;width: 120px;">
+      <h1 class="headder1">Link</h1>
+      <p class="text2">HOME</p>
+      <p class="text2">FEATURES</p>
+      <p class="text2">PRODUCTS</p>
+      <p class="text2">TESTIMONIAL</p>
+      <p class="text2">CONTACT</p>
+    </div>
+    <div class="columnfoot" style="padding-left:20px;width: 150px;">
+    <h1 class="headder1">Work</h1>
+      <p class="text2">NEW YORK ARENA</p>
+      <p class="text2">EAGLE PARK</p>
+      <p class="text2">NATIONALS PARK</p>
+      <p class="text2">MANILA PARK</p>
+    </div>
+    <div class="columnfoot" style="padding-left:50px;width: 230px;">
+      <input type="text" id="email" name="email" placeholder="Email">
+      <input type="submit" value="SEND">
+    </div>
+  </div>
+</div>
+
+<!-- License Web -->
+<div class="lc rowfoot">
+  <div class="rowf">
+  <div class="columnlc">
+      <p class="txtlc1">@2016 Free HTML5.All Rights Reserved.</p>
+  </div>
+  <div class="columnlc" style="padding-top: 20px;">
+      <a class="txtlc2">Designed by</a>
+      <a class="font" style="color:#2EAC6D;">FreeHTML5.co </a>
+      <a class="txtlc2">Demo Image: </a> 
+      <a class="font" style="color:#2EAC6D;">Pixeden</a>
+      <a class="txtlc2"> & </a>
+      <a class="font" style="color:#2EAC6D;"> Unsplash</a>
+  </div>
+    </div>
 </div>
 
 <!--Script Slide Image Architecture-->
@@ -112,33 +249,37 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";  
 }
-/*Slide Picture2*/
-var slideIndex2 = 1;
-showSlides2(slideIndex2);
 
-function plusSlides2(n) {
-  showSlides2(slideIndex2 += n);
-}
+var right = 0;
+var maxMargin;
+var jumpMargin = 100;
 
-function currentSlide2(n) {
-  showSlides2(slideIndex2 = n);
-}
-
-function showSlides2(n) {
-  var i;
-  var slides = document.getElementsByClassName("SlideImage2");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex2 = 1}    
-  if (n < 1) {slideIndex2 = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+function slideLeft(event){
+  var rowcont = document.querySelector(".row-container");
+  if(right <= -maxMargin){
+    event.preventDefault();
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+  else{
+    right -= jumpMargin;
   }
-  slides[slideIndex2-1].style.display = "block";  
-  dots[slideIndex2-1].className += " active";
+   rowcont.style.marginLeft = right+"px";
 }
+
+function slideRight(event){
+  var rowcont = document.querySelector(".row-container");
+  if(right==0){
+    event.preventDefault();
+  }
+  else if(right >= maxMargin){
+    event.preventDefault();
+  }
+  else{
+     right += jumpMargin;
+  }
+  rowcont.style.marginLeft = right+"px";
+}
+
+/*window.onload=setWidth;*/
 
 </script>
 
