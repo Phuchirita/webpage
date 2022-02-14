9070b360c4200722df8f496941efbbaa7ab8cbc5
Phuchirita <phuchirita.b@gmail.com>
2022-02-14 09:18:01 +0700

    commit

--- webpage.php
+++ webpage.php
@@ -205,7 +205,6 @@ function plusSlides(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("SlideImage1");
-  var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
